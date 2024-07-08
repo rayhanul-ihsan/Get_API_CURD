dd243bb048bf54e5d7a76266ef29b8e7e7ef5082
rayhanul-ihsan <mrayhanulihsan@gmail.com>
2024-07-08 08:00:43 +0700

    deploy ribettt yah

--- components/DeleteModal.tsx
+++ components/DeleteModal.tsx
@@ -101,7 +101,7 @@ const DeleteModal: React.FC<DeleteModalProps> = ({ id, onDeleteSuccess }) => {
                 >Yes</button>
                 <button
                   onClick={handleCloseModal}
-                  className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  className='py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
                 >No, cancel</button>
               </div>
             </div>
