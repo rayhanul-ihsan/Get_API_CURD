dac99fd497fbb4f2670e9e962c56ed0b5801b9ca
rayhanul-ihsan <mrayhanulihsan@gmail.com>
2024-07-08 07:51:18 +0700

    deploy ribet

--- components/DeleteModal.tsx
+++ components/DeleteModal.tsx
@@ -94,21 +94,15 @@ const DeleteModal: React.FC<DeleteModalProps> = ({ id, onDeleteSuccess }) => {
                     d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                   />
                 </svg>
-                <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                  Are you sure you want to delete this product?
-                </h3>
+                <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
                 <button
                   onClick={handleDelete}
                   className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
-                >
-                  Yes
-                </button>
+                >Yes</button>
                 <button
                   onClick={handleCloseModal}
                   className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                >
-                  No, cancel
-                </button>
+                >No, cancel</button>
               </div>
             </div>
           </div>
